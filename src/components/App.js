2dcb440f9c38504ea67ecc99e41188c16f49e0fa
janeljs <jisunlim818@gmail.com>
2021-01-31 23:45:10 +0900

    Add a new title

--- src/components/App.js
+++ src/components/App.js
@@ -3,30 +3,30 @@ import '../assets/App.css';
 import * as API from '../api'
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
-      name: 'junsoo',
+      name: 'Jane',
       posts: []
     }
   }
 
-  async componentDidMount(){
-    await API.getPost(1)
+  async componentDidMount() {
+    await API.getPost(1) // id를 가진 post만 반환
     await API.getPosts()
-    await API.modifyPost(1, {title: "new title", content: "new content"})
-    await API.createPost({title: "created post", content: "created content"})
+    await API.modifyPost(1, { title: "new title", content: "new content" })
+    await API.createPost({ title: "created post", content: "created content" }) // 포스트 생성
     await API.deletePost(3)
     await API.getPosts()
   }
 
-  render(){
+  render() {
     return (
       <div className="main">
         <div className="post-list"></div>
         <div className="post-body">
-          <p>Title</p>
-          <p>Content {this.state.name}</p>
+          <p><h1>First React Project</h1></p>
+          <p>published by {this.state.name}</p>
         </div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -5,4 +5,6 @@ import App from './components/App';
 ReactDOM.render(
   <App />,
   document.getElementById('root')
+
+
 );
